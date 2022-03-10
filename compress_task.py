37ca23912b29e251c78c37d0bec87eeb3f12a5a3
root <root@OJS-08.metabiblioteca.com.co>
2022-03-10 17:12:25 -0500

    Add SIZE to environment variables

--- compress_task.py
+++ compress_task.py
@@ -1,12 +1,19 @@
+#!/usr/bin/env python3
+
 from image_compressor import ImageCompressor
 import logging
 from os.path import join
 import datetime as dt
+from dotenv import dotenv_values
+from os.path import join, dirname
 
 def main():
-    logging.basicConfig(filename='compressor.log', level=logging.INFO)
+    dotenv_path = join(dirname(__file__), '.env')
+    config = dotenv_values(dotenv_path=dotenv_path)
+
+    logging.basicConfig(filename=config.get('LOG_FILE_PATH'), level=logging.INFO)
     logging.info(f'[{dt.datetime.now()}] Compression task started.')
-    compressor = ImageCompressor()
+    compressor = ImageCompressor(config)
     logging.info(f'[{dt.datetime.now()}] Compression task finished.')
 
 if __name__ == '__main__':
--- image_compressor.py
+++ image_compressor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import logging
 from PIL import Image
 from dotenv import dotenv_values
@@ -6,33 +7,34 @@ from os.path import join, dirname
 import datetime as dt
 
 
-dotenv_path = join(dirname(__file__), '.env')
-config = dotenv_values(dotenv_path=dotenv_path)
-SIZE = (450, 580)
-
-
 class ImageCompressor():
-    def __init__(self) -> None:
+    def __init__(self, config:dict) -> None:
+        self.config = config
         self.optimize_images()
-
+        
     def check_env(self):
         """
         Check if a .env file exists in folder and contains required environment variables.
         """
-        if config.get('OJS_DIR') == None or config.get('IMAGES_PATH') == None:
+        if not self.config.get('OJS_DIR') or not self.config.get('IMAGES_PATH'):
             logging.critical('There is no a valid .env file in folder.')
             raise FileNotFoundError('There is no a valid .env file in folder.')
+        if not self.config.get('SIZE') or not ',' in self.config.get('SIZE'):
+            logging.critical("Invalid configuration for SIZE parameter. Must have format 'height_dim, width_dim'.")
+            raise NameError("Invalid configuration for SIZE parameter. Must have format 'height_dim, width_dim'.")
+        else:
+            self.size = tuple(int(dimension) for dimension in self.config.get('SIZE').split(','))
 
     def get_platforms_dirs(self) -> list:
         """
         Returns a list of all platforms installed in OJS_DIR. If folder not exists, it catches the error and returns a message.
         """
         try:
-            os.listdir(config.get('OJS_DIR'))
+            os.listdir(self.config.get('OJS_DIR'))
         except FileNotFoundError:
-            logging.error(f"[{dt.datetime.now()}] There are no platforms in {config.get('OJS_DIR')}.")
+            logging.error(f"[{dt.datetime.now()}] There are no platforms in {self.config.get('OJS_DIR')}.")
         else:
-            return os.listdir(config.get('OJS_DIR'))
+            return os.listdir(self.config.get('OJS_DIR'))
 
     def optimize_images(self) -> None:
         """
@@ -48,7 +50,7 @@ class ImageCompressor():
         Executes the compression of images in the IMAGES_PATH of a directory in OJS_DIR. If journals folder not exist, it catches the error and returns a message.
         """
 
-        path = f"{config.get('OJS_DIR')}/{directory}/{config.get('IMAGES_PATH')}"
+        path = f"{self.config.get('OJS_DIR')}/{directory}/{self.config.get('IMAGES_PATH')}"
 
         try:
             os.listdir(path)
@@ -59,13 +61,14 @@ class ImageCompressor():
             logging.info(f"[{dt.datetime.now()}] Checking folder {directory}.")
             if folders:
                 for folder in folders:
+                    logging.info(f"[{dt.datetime.now()}] Checking journal {folder}.")
                     files = os.listdir(f"{path}{folder}")
                     covers = [file for file in files if file.startswith(
                         'cover_issue') and (file.endswith('png') or file.endswith('jpg'))]
                     if covers:
                         for image in covers:
                             with Image.open(f'{path}{folder}/{image}') as img:
-                                if img.size > SIZE:
-                                    img = img.resize(SIZE)
+                                if img.size > self.size:
+                                    img = img.resize(self.size)
                                     img.save(f'{path}{folder}/{image}')
                                     logging.info(f"[{dt.datetime.now()}] Image {image} compressed.")
