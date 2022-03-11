13587fe8aaca8322bd9637c64871abee23174f35
Luis Moreno <pipemoreno9405@gmail.com>
2022-03-10 22:25:50 -0500

    Add environment variable for log file

--- compress_task.py
+++ compress_task.py
@@ -7,14 +7,22 @@ import datetime as dt
 from dotenv import dotenv_values
 from os.path import join, dirname
 
+
 def main():
     dotenv_path = join(dirname(__file__), '.env')
     config = dotenv_values(dotenv_path=dotenv_path)
 
-    logging.basicConfig(filename=config.get('LOG_FILE_PATH'), level=logging.INFO)
-    logging.info(f'[{dt.datetime.now()}] Compression task started.')
-    compressor = ImageCompressor(config)
-    logging.info(f'[{dt.datetime.now()}] Compression task finished.')
+    if config.get('LOG_FILE_PATH'):
+        logging.basicConfig(filename=config.get(
+            'LOG_FILE_PATH'), level=logging.INFO)
+        logging.info(f'[{dt.datetime.now()}] Compression task started.')
+        compressor = ImageCompressor(config)
+        logging.info(f'[{dt.datetime.now()}] Compression task finished.')
+
+    else:
+        raise FileNotFoundError(
+            ".env config file doesn't exist or there is not a LOG_FILE_PATH value set.")
+
 
 if __name__ == '__main__':
     main()
--- image_compressor.py
+++ image_compressor.py
@@ -1,46 +1,53 @@
 #!/usr/bin/env python3
 import logging
 from PIL import Image
-from dotenv import dotenv_values
 import os
-from os.path import join, dirname
 import datetime as dt
 
 
 class ImageCompressor():
-    def __init__(self, config:dict) -> None:
-        self.config = config
+    def __init__(self, config: dict) -> None:
+        self.check_env(config)
         self.optimize_images()
-        
-    def check_env(self):
+
+    def check_env(self, config: dict):
         """
         Check if a .env file exists in folder and contains required environment variables.
         """
-        if not self.config.get('OJS_DIR') or not self.config.get('IMAGES_PATH'):
-            logging.critical('There is no a valid .env file in folder.')
-            raise FileNotFoundError('There is no a valid .env file in folder.')
-        if not self.config.get('SIZE') or not ',' in self.config.get('SIZE'):
-            logging.critical("Invalid configuration for SIZE parameter. Must have format 'height_dim, width_dim'.")
-            raise NameError("Invalid configuration for SIZE parameter. Must have format 'height_dim, width_dim'.")
+        if not config.get('OJS_DIR') or not config.get('IMAGES_PATH'):
+            logging.critical(
+                f'[{dt.datetime.now()}] There is not an OJS_DIR OR IMAGES_PATH value in .env file.')
+            raise NameError(
+                'There is not an OJS_DIR OR IMAGES_PATH value in .env file.')
         else:
-            self.size = tuple(int(dimension) for dimension in self.config.get('SIZE').split(','))
+            self.ojs_dir = config.get('OJS_DIR')
+            self.images_path = config.get('IMAGES_PATH')
+
+        if not config.get('SIZE') or not ',' in config.get('SIZE'):
+            logging.critical(
+                f"[{dt.datetime.now()}] Invalid configuration for SIZE parameter. Must have format 'width_dim,height_dim'.")
+            raise NameError(
+                "Invalid configuration for SIZE parameter. Must have format 'width_dim,height_dim'.")
+        else:
+            self.size = tuple(int(dimension)
+                              for dimension in config.get('SIZE').split(','))
 
     def get_platforms_dirs(self) -> list:
         """
         Returns a list of all platforms installed in OJS_DIR. If folder not exists, it catches the error and returns a message.
         """
         try:
-            os.listdir(self.config.get('OJS_DIR'))
+            os.listdir(self.ojs_dir)
         except FileNotFoundError:
-            logging.error(f"[{dt.datetime.now()}] There are no platforms in {self.config.get('OJS_DIR')}.")
+            logging.error(
+                f"[{dt.datetime.now()}] There are no platforms in {self.ojs_dir}.")
         else:
-            return os.listdir(self.config.get('OJS_DIR'))
+            return os.listdir(self.ojs_dir)
 
     def optimize_images(self) -> None:
         """
         Initializes the compression task of images in the IMAGES_PATH of each platform in OJS_DIR.
         """
-        self.check_env()
         if self.get_platforms_dirs():
             for _dir in self.get_platforms_dirs():
                 self.compress(directory=_dir)
@@ -50,18 +57,20 @@ class ImageCompressor():
         Executes the compression of images in the IMAGES_PATH of a directory in OJS_DIR. If journals folder not exist, it catches the error and returns a message.
         """
 
-        path = f"{self.config.get('OJS_DIR')}/{directory}/{self.config.get('IMAGES_PATH')}"
+        path = f"{self.ojs_dir}/{directory}/{self.images_path}"
 
         try:
             os.listdir(path)
         except FileNotFoundError:
-            logging.error(f"[{dt.datetime.now()}] No journals folder found in {directory}.")
+            logging.error(
+                f"[{dt.datetime.now()}] No journals folder found in {directory}.")
         else:
             folders = os.listdir(path)
             logging.info(f"[{dt.datetime.now()}] Checking folder {directory}.")
             if folders:
                 for folder in folders:
-                    logging.info(f"[{dt.datetime.now()}] Checking journal {folder}.")
+                    logging.info(
+                        f"[{dt.datetime.now()}] Checking journal {folder}.")
                     files = os.listdir(f"{path}{folder}")
                     covers = [file for file in files if file.startswith(
                         'cover_issue') and (file.endswith('png') or file.endswith('jpg'))]
@@ -71,4 +80,5 @@ class ImageCompressor():
                                 if img.size > self.size:
                                     img = img.resize(self.size)
                                     img.save(f'{path}{folder}/{image}')
-                                    logging.info(f"[{dt.datetime.now()}] Image {image} compressed.")
+                                    logging.info(
+                                        f"[{dt.datetime.now()}] Image {image} compressed.")
